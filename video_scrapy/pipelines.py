6ac0a2a1a51d6a231444ff1d604ed041755dd022
邓景 <dengjing@dengjingdeMacBook-Pro.local>
2015-09-30 19:15:29 +0800

    总算是通了

--- video_scrapy/pipelines.py
+++ video_scrapy/pipelines.py
@@ -6,21 +6,24 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 from pymongo import MongoClient
-from video_scrapy.settings import MONGODB_SERVER
-from video_scrapy.settings import MONGODB_USER
-from video_scrapy.settings import MONGODB_PORT
-from video_scrapy.settings import MONGODB_PASSWORD
+# from video_scrapy.settings import MONGODB_SERVER
+# from video_scrapy.settings import MONGODB_USER
+# from video_scrapy.settings import MONGODB_PORT
+# from video_scrapy.settings import MONGODB_PASSWORD
 
 
 class VideoScrapyPipeline(object):
 
     def __init__(self):
         # client = MongoClient('mongodb://'+ MONGODB_USER + ':' + MONGODB_PASSWORD + '@' + MONGODB_SERVER, MONGODB_PORT)
-        # client = MongoClient('mongodb://root:dengjing@oforever.net:27017/video_scrapy')
-        client = MongoClient('mongodb://root:dengjing@123.57.6.46:27017/')
-        client.database.authenticate("root", "dengjing")
-        db = client.database
-        self.collection = db.collection
+        db = MongoClient('123.57.6.46')
+        # client = MongoClient('mongodb://123.57.6.46:27017/')
+        # client = MongoClient('mongodb://root:dengjing@123.57.6.46:27017/')
+        db.video_scrapy.authenticate("scrapy", "123456")
+        # db = client.database
+        # self.collection = client.collection
+        print('!!!!!!!!', db.video_scrapy.base_video.find_one())
+        pass
 
     def process_item(self, item, spider):
         print('--------收录视屏-------')
