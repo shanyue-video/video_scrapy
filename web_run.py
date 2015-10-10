d38e5b42cee61cd6c8218291dea26368dd3fad0b
dengjing <554816284@qq.com>
2015-10-10 17:02:54 +0800

    .

--- web_run.py
+++ web_run.py
@@ -3,6 +3,7 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 import tornado
 import tornado.options
+import tornado.web
 
 __author__ = 'dengjing'
 
