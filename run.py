9624ffff1bdec22f67eff2ac8199fdf2ef84a4c4
dengjing <554816284@qq.com>
2015-10-10 16:32:32 +0800

    新版本的代码方式启动scrapy

--- run.py
+++ run.py
@@ -1,7 +1,4 @@
-from scrapy import log
-from scrapy import signals
-from scrapy.crawler import Crawler
-from scrapy.utils import reactor
+from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
 __author__ = 'dengjing'
@@ -9,29 +6,10 @@ __author__ = 'dengjing'
 
 class manage:
 
-    spiderCounter = 0
-
-    def spiderClosed(self):
-        self.spiderCounter -= 1
-        if self.spiderCounter == 0:
-            reactor.stop()
-
-    def setupCrawler(self, spiderName):
-        crawler = Crawler(get_project_settings())
-        crawler.signals.connect(self.spiderClosed, signal=signals.spider_closed)
-        crawler.configure()
-        log.start()
-        spider = crawler.spiders.create(spiderName)
-        crawler.crawl(spider)
-        crawler.start()
-
     def run(self):
-        crawler = Crawler(get_project_settings())
-        crawler.configure()
-        for spiderName in crawler.spiders.list():
-            self.spiderCounter += 1
-            self.setupCrawler(spiderName)
-        reactor.run()
+        process = CrawlerProcess(get_project_settings())
+        process.crawl('iqiyi')
+        process.start()
 
 
 if __name__ == '__main__':
