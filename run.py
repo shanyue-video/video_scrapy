843892853e7410e1587f870f4ca08dff3e45971a
dengjing <554816284@qq.com>
2015-10-12 17:43:03 +0800

    .

--- run.py
+++ run.py
@@ -1,21 +1,15 @@
-from scrapy.crawler import CrawlerProcess
+from scrapy.crawler import CrawlerRunner
 from scrapy.utils.project import get_project_settings
 import time
+from twisted.internet import reactor
 
 __author__ = 'dengjing'
 
 
-# class manage:
-#
-#     def run(self):
-#         process = CrawlerProcess(get_project_settings())
-#         process.crawl('iqiyi')
-#         process.start()
-
-
 if __name__ == '__main__':
     while True:
-        process = CrawlerProcess(get_project_settings())
-        process.crawl('iqiyi')
-        process.start()
+        runner = CrawlerRunner(get_project_settings())
+        d = runner.crawl('iqiyi')
+        d.addBoth(lambda _: reactor.stop())
+        reactor.run()
         time.sleep(3)
