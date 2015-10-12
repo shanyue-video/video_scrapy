ec5566ae6159ef802aec7952c06c0ab6e8926242
dengjing <554816284@qq.com>
2015-10-12 17:33:56 +0800

    .

--- run.py
+++ run.py
@@ -5,16 +5,17 @@ import time
 __author__ = 'dengjing'
 
 
-class manage:
-
-    def run(self):
-        process = CrawlerProcess(get_project_settings())
-        process.crawl('iqiyi')
-        process.start()
+# class manage:
+#
+#     def run(self):
+#         process = CrawlerProcess(get_project_settings())
+#         process.crawl('iqiyi')
+#         process.start()
 
 
 if __name__ == '__main__':
     while True:
-        manage = manage()
-        manage.run()
+        process = CrawlerProcess(get_project_settings())
+        process.crawl('iqiyi')
+        process.start()
         time.sleep(3)
