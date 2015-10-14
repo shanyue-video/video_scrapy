6d75612ec28616dbf79c55f942586d2e916e18f0
dengjing <554816284@qq.com>
2015-10-14 16:07:37 +0800

    使用twisted的方式自己实现

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from scrapy.crawler import CrawlerRunner
+from twisted.internet import defer
 from scrapy.utils.project import get_project_settings
 import time
 from twisted.internet import reactor
@@ -7,9 +8,12 @@ __author__ = 'dengjing'
 
 
 if __name__ == '__main__':
-    while True:
-        runner = CrawlerRunner(get_project_settings())
-        d = runner.crawl('iqiyi')
-        d.addBoth(lambda _: reactor.stop())
-        reactor.run()
-        time.sleep(3)
+    runner = CrawlerRunner(get_project_settings())
+
+    @defer.inlineCallbacks
+    def crawl():
+        while True:
+            yield runner.crawl('iqiyi')
+
+    crawl()
+    reactor.run()
